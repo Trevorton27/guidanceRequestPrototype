023331a7c1ab98764194fc1fe2ba6b6a88ec6297
Trevor Mearns <spiral272@gmail.com>
2021-06-17 21:27:22 +0900

    Updates

---
+++ precache-manifest.325b804ecf235da9e87e2c6bf85c02b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea1874e3eed19a7c9c7d70220d6560d",
+    "url": "/guidanceRequestPrototype/index.html"
+  },
+  {
+    "revision": "f1000ea6174d10417104",
+    "url": "/guidanceRequestPrototype/static/css/2.cf2a111a.chunk.css"
+  },
+  {
+    "revision": "3141bea4f6bdb3f0f963",
+    "url": "/guidanceRequestPrototype/static/css/main.1859f932.chunk.css"
+  },
+  {
+    "revision": "f1000ea6174d10417104",
+    "url": "/guidanceRequestPrototype/static/js/2.b021b1e5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/guidanceRequestPrototype/static/js/2.b021b1e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3141bea4f6bdb3f0f963",
+    "url": "/guidanceRequestPrototype/static/js/main.142a0eb0.chunk.js"
+  },
+  {
+    "revision": "770afb4aeb8469988750",
+    "url": "/guidanceRequestPrototype/static/js/runtime-main.88ea6fd3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/guidanceRequestPrototype.github.io/guidanceRequestPrototype/precache-manifest.a00449627a9262765c623fe4ed28206f.js"
+  "/guidanceRequestPrototype/precache-manifest.325b804ecf235da9e87e2c6bf85c02b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/guidanceRequestPrototype.github.io/guidanceRequestPrototype/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/guidanceRequestPrototype/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
